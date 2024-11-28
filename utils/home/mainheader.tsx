05fc5c968204d66f44e144a6bc7222161a22720f
elicatinthebox <elisaromondia@gmail.com>
2024-11-28 13:32:17 +0100

    Update mainheader.tsx

--- utils/home/mainheader.tsx
+++ utils/home/mainheader.tsx
@@ -9,9 +9,11 @@ export function MainHeader() {
         {useTypedText("Elisa Romondia", 50)}
         <br />
       </h1>
-      <h2 >
-        {"Certified Cloud Solution Architect and Data Scientist, internationally recognized as one of the Forbes 60 women who are shaking up tech across the globe. Expert in designing and implementing distributed architectures and data infrastructures."}
-      </h2>
+      <h2>
+    <a href="https://learn.microsoft.com/api/credentials/share/it-it/ElisaRomondia-2516/803D995FBA8EDF8A?sharingId=F844649855895FD0">
+        Certified Cloud Solution Architect and Data Scientist, internationally recognized as one of the Forbes 60 women who are shaking up tech across the globe. Expert in designing and implementing distributed architectures and data infrastructures.
+    </a>
+</h2>
     </div>
   )
 }
