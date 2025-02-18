b9ead1e12966a36e97e9985726ad74c9a66f9c37
elicatinthebox <elisaromondia@gmail.com>
2025-02-18 19:14:53 +0100

    Update mainheader.tsx

--- utils/home/mainheader.tsx
+++ utils/home/mainheader.tsx
@@ -11,7 +11,7 @@ export function MainHeader() {
       </h1>
       <h2>
     <a href="https://learn.microsoft.com/api/credentials/share/it-it/ElisaRomondia-2516/803D995FBA8EDF8A?sharingId=F844649855895FD0">
-        Certified Cloud Solution Architect</a> and Data Scientist, internationally recognized as one of the Forbes 60 women who are shaking up tech across the globe. Expert in designing and implementing distributed architectures and data infrastructures.
+        Certified Cloud Solution Architect</a> and <a href="https://learn.microsoft.com/api/credentials/share/en-us/ElisaRomondia-2516/EAC2123539ADA800?sharingId=F844649855895FD0">Data Scientist</a>, internationally recognized as one of the Forbes 60 women who are shaking up tech across the globe. Expert in designing and implementing distributed architectures and data infrastructures.
 </h2>
     </div>
   )
